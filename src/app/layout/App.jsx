e06fee44cebf42d560274e3f4e886e010462ec40
david.onyimbo@gmail.com <remix2344>
2018-06-16 11:06:23 +0300

    Boilerplate Front end

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/EventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar/NavBar";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Deventus</h1>
+        <NavBar />
+        <Container className="main">
+          <h1>Deventus</h1>
+
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
 }
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
-import App from './app/layout/App.jsx';
+import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
 
-const root=document.getElementById('root')
+const root = document.getElementById('root')
 
-let render=()=>{
-    ReactDOM.render(<App />, root);
+let render = () => {
+    ReactDOM.render( < App /> , root);
 }
 
-if(module.hot){
-    module.hot.accept('./app/layout/App',()=>{
+if (module.hot) {
+    module.hot.accept('./app/layout/App', () => {
         setTimeout(render);
     });
 }
